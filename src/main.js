3e09e9675e10d59a9a7648c62234b85fc7a87033
Peter Pajchl <peter.pajchl@gmail.com>
2025-09-19 00:31:00 +0100

    starting some UI

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ console.log("Wonderling is running...");
 
 import { LitElement, html } from "lit";
 
+async function fetchCountries() {
+  try {
+    let res = await fetch("http://localhost:4123/api/countries?page=0");
+    let data = await res.json();
+    console.log("Data", data);
+    return data;
+  } catch (e) {
+    console.log("Error with request", e);
+  }
+}
+
 class WanderlingApp extends LitElement {
   createRenderRoot() {
     return this; // Disable shadow DOM
@@ -12,15 +23,7 @@ class WanderlingApp extends LitElement {
   }
 
   render() {
-    const countryObj = {
-      name: "Czechia",
-      countryCode: "cz",
-      capital: "Prague",
-    };
-    return html`
-      <h1>Wanderling</h1>
-      <country-view .countryObj=${countryObj} />
-    `;
+    return html`Learn 197 countries and its capital city...`;
   }
 }
 
@@ -46,6 +49,49 @@ class Country extends LitElement {
         />
       </div>
       <div>${this.countryObj.capital} <audio-player /></div>
+    `;
+  }
+}
+
+class CountryList extends LitElement {
+  static properties = {
+    data: { type: Array },
+  };
+
+  constructor() {
+    super();
+    this.data = [];
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    fetchCountries().then((d) => {
+      this.data = d;
+    });
+  }
+
+  render() {
+    console.log(this.data);
+    return html`
+      <ul>
+        ${this.data.map(
+          (item) => html`
+            <li>
+              <div>${item.country}</div>
+              <div>${item.capital}</div>
+              <div>
+                <img
+                  src="/static/flags/w320-webp/${item.country_code.toLowerCase()}.webp"
+                />
+              </div>
+            </li>
+          `,
+        )}
+      </ul>
     `;
   }
 }
@@ -93,6 +139,7 @@ class AudioPlayer extends LitElement {
   }
 }
 
+customElements.define("country-list", CountryList);
 customElements.define("audio-player", AudioPlayer);
 customElements.define("country-view", Country);
 customElements.define("wanderling-app", WanderlingApp);
